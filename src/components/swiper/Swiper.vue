3c4ec0bb44e5fcb2c45728c4a8d5c10602cbcd2a
Hogan <hogan@dcpt.online>
2020-05-31 18:17:51 +0800

    menu

--- src/components/swiper/Swiper.vue
+++ src/components/swiper/Swiper.vue
@@ -39,7 +39,7 @@ export default {
     width: 100%
     overflow: hidden
     height: 0
-    padding-bottom: 37.87%
+    padding-bottom: 37.15%
     background: #eee
     .swiper-img
       width: 100%
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,18 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
-    <div>test</div>
+    <home-menu></home-menu>
   </div>
 </template>
 
 <script>
 import HomeHeader from '../../components/header/Header'
 import HomeSwiper from '../../components/swiper/Swiper'
+import HomeMenu from '../../components/menu/Menu'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeMenu
   }
 }
 </script>
